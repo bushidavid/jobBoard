d4f2578b7d23089bc8ac2fd72758d8fb2c1a1600
davidbushi <88148058+davidbushi@users.noreply.github.com>
2022-12-12 23:29:19 +0100

    Tailwind CSS, Login.js, React Routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import Navbar from './Navbar';
 import Home from './Home';
 import InputJob from './InputJob';
 import JobList from "./JobList";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from './Login';
 
 function App() {
   
   const title = 'Welcome to the New Joab Board';
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <InputJob />
-        <JobList />
-      </div>
-    </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+
+              <div className="content">
+                <InputJob />
+                <JobList />
+              </div>
+
+        </div>
+      </Router>
   );
 }
 
