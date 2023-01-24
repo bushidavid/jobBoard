603c5a896b253e1236f08129990bdde96a222c9d
davidbushi <88148058+davidbushi@users.noreply.github.com>
2023-01-24 20:30:35 +0100

    made changes to improve Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,11 @@ import React from "react";
 
 const Navbar = () => {
     return (  
-        <nav className="navbar">
-            <h1><a href="/home">Remotify Europe</a></h1>
-            <div className="links">
+        <nav className="navbar flex p-5 justify-between max-w-4xl place-self-center">
+            <div className="">
+                <h1><a href="/home">Remotify Europe</a></h1>
+            </div>
+            <div>
                 <a href="/login">Log In</a>
                 <a href="/create-job" style={{
                     color: "white",
